c61a7d57a704c38ec4ec4c71851788c7657d32b3
JohnCoene <jcoenep@gmail.com>
2019-08-31 19:27:53 +0200

    agriculture data for examples

--- R/data.R
+++ R/data.R
@@ -5,8 +5,21 @@
 #' @format A data.frame object with 178 rows and 8 columns
 #' \describe{
 #'   \item{\code{start_lat}, \code{start_lon}, \code{end_lat}, \code{end_lon}}{: geographical coordinates.}
-#'   \item{\code{airline}}{; name of airline}
+#'   \item{\code{airline}}{: name of airline.}
 #'   \item{\code{airport_start}, \code{airport_end}}{: names of airports.}
-#'   \item{\code{cnt}}{: number of flights}
+#'   \item{\code{cnt}}{: number of flights.}
 #' }
 "usflights"
+
+#' Agricultural Lands
+#'
+#' A datatset of the available agricultural land (as percentage of land area) by country in 2016.
+#'
+#' @format A data.frame object with 254 rows and 3 columns
+#' \describe{
+#'   \item{\code{country_name}}{: name of the country.}
+#'   \item{\code{country_code}}{: the iso3 code identifying the country.}
+#'   \item{\code{percent}}{: percentage of lang.}
+#' }
+#' @source \url{https://data.worldbank.org/indicator/AG.LND.AGRI.ZS}
+"agriland"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,4 @@
+# sample dataset for arcs
 usflights <- read.csv(
   paste0("https://raw.githubusercontent.com/plotly/datasets/",
          "master/2011_february_aa_flight_paths.csv")
@@ -6,4 +7,47 @@ usflights <- read.csv(
 names(usflights) <- c("start_lat", "start_lon", "end_lat", "end_lon", "airline", 
   "airport_start", "airport_end", "cnt")
 
-usethis::use_data(usflights, overwrite = TRUE)
+# countries polygons
+library(purrr)
+
+# read json to list
+countries_geojson <- geojsonio::geojson_read("https://raw.githubusercontent.com/vasturiano/globe.gl/master/example/datasets/ne_110m_admin_0_countries.geojson")
+countries_features <- countries_geojson$features # get features only
+
+# get iso3
+country_iso3 <- map(countries_features, "properties") %>% 
+  map("ISO_A3") %>% 
+  unlist()
+
+country_iso2 <- map(countries_features, "properties") %>% 
+  map("ISO_A2") %>% 
+  unlist()
+
+country_name <- map(countries_features, "properties") %>% 
+  map("NAME_SORT") %>% 
+  unlist()
+
+country_polygons <- tibble::tibble(
+  country_name = country_name,
+  country_iso3 = country_iso3,
+  country_iso2 = country_iso2,
+  features = countries_features
+)
+
+# agricultural lands
+library(dplyr)
+
+agriland <- read.csv("./data-raw/agricultural_land_area.csv", skip = 4, stringsAsFactors = FALSE)
+agriland <- agriland %>% 
+  select(
+    country_name = `Country.Name`,
+    country_code = `Country.Code`,
+    percent = X2016
+  ) %>% 
+  mutate(
+    percent = percent / 100
+  ) %>% 
+  filter(!is.na(percent))
+
+usethis::use_data(usflights, agriland, overwrite = TRUE)
+usethis::use_data(country_polygons, internal = TRUE, overwrite = TRUE)
