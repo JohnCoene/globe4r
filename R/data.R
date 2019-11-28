c3d97200b14b18aa40ea7b4d2cf08e578608350b
JohnCoene <jcoenep@gmail.com>
2019-11-28 23:44:25 +0100

    cables data source added

--- R/data.R
+++ R/data.R
@@ -41,4 +41,6 @@
 #' Underwater cables.
 #'
 #' @format An object of class sp containing LINESTRINGS.
+#' 
+#' @source \url{https://raw.githubusercontent.com/telegeography/www.submarinecablemap.com/master/public/api/v2/cable/cable-geo.json}
 "cables"
